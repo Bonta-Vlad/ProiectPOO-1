282b46ac069e7bf95b88b1c820f921f8ff45382a
bonta-vlad <bontavladgabriel@gmail.com>
2026-04-06 14:10:10 +0300

    another exception

--- engines.cpp
+++ engines.cpp
@@ -1,7 +1,11 @@
+#include "exceptions.h"
 #include "engines.h"
 #include <cmath>
 //displacement formula taken from wikipedia
 double Reciprocating::displacement() const{
+    if(bore==0){
+        throw MathError("Dividing by zero in displacement calculation![Rotary]");
+    }
                                       //pi(bore/2)^2
     return stroke_length*cylinder_count*2*acos(0.0)*pow((bore/2),2);
 }
--- exceptions.h
+++ exceptions.h
@@ -6,3 +6,10 @@ class InvalidInput: public std::exception{
     explicit InvalidInput(const std::string& msg): message(msg){}
     const char* what() const noexcept;
 };
+
+class MathError: public std::exception{
+    std::string message;
+    public:
+    explicit MathError(const std::string& msg): message(msg){}
+    const char* what() const noexcept;
+};
